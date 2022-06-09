def9c461d6d0f2ffa040064083b734547c1eb2db
James Penn <james@jamespenn.co.uk>
2022-06-09 13:30:43 +0100

    updates to running ensemble

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -4,23 +4,23 @@
 #SBATCH --qos=normal
 #SBATCH --time=200
 #SBATCH --mem=32G
-#SBATCH --ntasks=26
-#SBATCH --output=/data/users/hadcam/qjob.out
+#SBATCH --ntasks=40
+#SBATCH --output=/data/users/%u/fair_ensemble_%j.out
 #SBATCH --signal=B:2@60
 
 # To submit this job at the command line you need to write on the command line $> sbatch submit_job.sh
-# I ran this once to make the config files and once to do the runs with co2 affecting temperature. 
-# Parallel to this I submitted the bgc_submit_job.sh to do the bgc runs where the CO2 is not allowed to affect temperature.  
+# I ran this once to make the config files and once to do the runs with co2 affecting temperature.
+# Parallel to this I submitted the bgc_submit_job.sh to do the bgc runs where the CO2 is not allowed to affect temperature.
 
-# $> sacct allows progress of job on spice to be monitored 
+# $> sacct allows progress of job on spice to be monitored
 # $> scancel allows you to cancel a job if you need to
 
-# Only one exec command will run at a time they won't run consecutively from one submit. 
+# Only one exec command will run at a time they won't run consecutively from one submit.
 # To minimise how long it takes to run it is recommended to copy this script and rename it and resubmit it for each set of configs.
 
 set -euo pipefail
-conda activate climate-outcomes
+conda activate fair-spice
 #exec python FaIR_inv_make_configs.py # to make the config files
-exec python FaIR_inv_run.py configsdir/std_configs.json ../data_output/std_inverse_results.npy 
+exec python scripts/run_ensemble.py --samples=100000 ensemble_full.nc
 #exec python FaIR_inv_run.py configsdir/bgc_configs.json data_output/bgc_inverse_results.np
 
