d4f59910961b198ecf8833decb3f66633669f968
Camilla Mathison <camilla.mathison@metoffice.gov.uk>
2022-06-17 12:35:57 +0100

    Updated readme and two new configs (#2)
    * Updated readme and two new configs
    
    * Update scripts/run_ensemble_bgc.py
    
    Co-authored-by: James Penn <james@jamespenn.co.uk>
    
    * Support for timeseries inputs other than C
    
    * Updates to include
    * More instructions to the readme file.
    * A new notebook to use the new xarray functionality to do the analysis for deriving alpha, beta and gamma
    * An bugfix to the bgc to make the other_rf a negative value.
    * A new notebooks to run FaIR using AR6 constraints for two ssps with relevance for the Methane pledges.
    * Include the ability to download the files for running AR6 version of FaIR from zenodo
    
    * Update README.md
    
    formatting fixes
    
    Co-authored-by: camilla.mathison <hadcam@vld393.cmpd1.metoffice.gov.uk>
    Co-authored-by: James Penn <james@jamespenn.co.uk>

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # FaIR-Spice
 A repository to run big ensembles of FaIR using Spice and save output and configs. 
+In this repository you will find example notebooks with manageable examples of running FaIR and examples to 
+run much bigger ensembles of runs on Spice using scripts.
+
+## Conda 
+
+The conda environment you need to run the code is in environment.yml
+
+Recommended to use python 3.7+ with anaconda.
+
+1. Navigate to your local working directory, then clone the repository
+```bash
+git clone https://github.com/camillamathison/FaIR-Spice.git
+cd Fair-Spice
+```
+
+2. Create environment:
+```bash
+conda env create -f environment.yml
+```
+
+3. To run ...
+```bash
+conda activate fair-spice
+```
+
+## The jupyter notebooks in /notebooks provides several example notebooks:
+
+- run_ensemble.ipynb 
+    - provides three options for running FaIR in the inverse set up from concentrations to diagnose emissions and provide a temperature.
+    - uses xarray to enable the addition of metadata so you can easily plot the data that comes out as shown in the xarray_analysis.ipynb notebook 
+- xarray_analysis.ipynb - notebook to plot the output
+- xarray_analysis_alphabetagamma_ensemble.ipynb - notebook to plot the output from a bgc and standard run to calculate alpha, beta and gamma
+- simple_forward.ipynb - a really simple example for running FaIR from emissions to give a concentration, forcing and temperature.  
+- simple_inverse.ipynb - a really simple example for running FaIR from concentrations to give emissions, forcing and temperature.  
+- run-fair-ar6-ssp245.ipynb 
+    - A more complicated example using the constrained AR6 set up of FaIR 1.6 to run two ssp scenarios (SSP2-45 and SSP3-60)
+    - These two scenarios are relevant to work done already on the Methane pledges .  
+
+
+
+## The code in /scripts provides a way of running much bigger ensembles directly on Spice
+ 
+**run_ensemble.py:**
+The configs for running FaIR are created in this file. you can run this using this command:
+
+e.g. running a sample size of 1000 you would use this command:
+```bash
+python scripts/run_ensemble.py --samples=1000 "fair_ensemble.nc"
+```
+
+You can change this file to specify the details of the run. 
+I recommend changing the name of this file to reflect the run you have set up                    
+                   
+e.g., An example of the commands you would use to run a 10000 member ensemble for a standard run where CO2 affects climate and 
+```bash
+python scripts/run_ensemble_std.py --samples=10000 "fair_ensemble_std.nc"
+```
+and perhaps to run an equivalent size ensemble where the CO2 is not allowed to affect climate: e.g.
+```bash
+python scripts/run_ensemble_bgc.py --samples=10000 "fair_ensemble_bgc.nc"
+```
+This would create two lots of output for similar runs but make sure one would not overwrite the other.
+I have used this to run a 1000000 runs on Spice which can be submitted using the submit_job.sh script below.
+                   
+**submit_job.sh:**
+The run_ensemble.py script can be submitted to spice using the sbatch command. 
+Edit this to do a different size ensemble and to make sure you don't overwrite your data.
+```bash
+sbatch submit_job.sh
+```
+
+
---
+++ scripts/run_ensemble_bgc.py
@@ -0,0 +1,122 @@
+
+
+from fair_spice.ensemble import make_member, stream_ensemble
+import fair_spice.config as config
+from fair_spice.fair.models import inverse_fair
+from fair.forcing.ghg import co2_log
+
+def run_model(params):
+    n, config = params
+    run = inverse_fair(config)
+    member = make_member(run, n, invariant=["C"])
+    return member
+
+def main():
+    import argparse
+    import multiprocessing
+    import os
+    import time
+    import sys
+
+    import numpy as np
+    import pandas as pd
+    import scipy.stats as st
+    from tqdm import tqdm
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("outfile")
+    # parser.add_argument("--partition", "-p")
+    # parser.add_argument("--partition-size", "-s")
+    parser.add_argument("--num-cores", "-n")
+    parser.add_argument("--samples", type=int, default=1000000)
+
+    args = parser.parse_args()
+
+    outfile = args.outfile
+
+    N_PROCS = args.num_cores or int(os.environ.get("SLURM_NTASKS", "0")) or os.cpu_count()
+    SEEDS = config.RANDOM_SEEDS
+    SAMPLES = args.samples
+    F2XCO2_MEAN = 4.00
+    F2XCO2_NINETY = 0.48
+    NINETY_TO_ONESIGMA = st.norm.ppf(0.95)
+
+    nt = 141
+    Cpi = 284.32
+    C = Cpi * 1.01 ** np.arange(nt)
+
+    r0_fs = st.uniform.rvs(
+        loc=27.7, scale=41.3 - 27.7, random_state=SEEDS[10], size=SAMPLES
+    )
+    rC_fs = st.uniform.rvs(loc=0, scale=0.0482, random_state=SEEDS[11], size=SAMPLES)
+    rT_fs = st.uniform.rvs(
+        loc=-0.0847, scale=4.52 + 0.0847, random_state=SEEDS[12], size=SAMPLES
+    )
+    pre_ind_co2 = st.norm.rvs(loc=277.147, scale=2.9, random_state=SEEDS[13], size=SAMPLES)
+
+    tunings_file = config.ROOT_DIR / "data_input/tunings/cmip6_twolayer_tuning_params.json"
+    column_order = ["q4x", "lamg", "cmix", "cdeep", "gamma_2l", "eff"]
+    tunings = config.load_tunings(tunings_file).drop(columns=["t4x"])[column_order]
+
+    print("First 5 model parameters:")
+    print(tunings.head())
+    print()
+
+
+    # Create a 6D Gaussian kernel based on the data from the model tunings
+    # and resample for the number of samples we want to cover
+    kde = st.gaussian_kde(tunings.T)
+    samples_raw = kde.resample(size=int(SAMPLES * 1.25), seed=SEEDS[15])
+
+    samples = pd.DataFrame(samples_raw.T, columns=tunings.columns)
+    # remove unphysical combinations
+    samples[samples <= 0] = np.nan
+    # lamg is always < 0. But this truncnorm approx seems to overwrite
+    # sample distribution that came from the kde? TODO: check
+    samples.lamg = st.truncnorm.rvs(
+        -2, 2, loc=-4 / 3, scale=0.5, size=int(SAMPLES * 1.25), random_state=SEEDS[16]
+    )
+
+    samples = samples.dropna(how="any")[:SAMPLES]
+    print("First 5 param samples")
+    print(samples.head())
+    print()
+
+    # create TCR and ECS random samples from the model distributions
+    f2x = st.norm.rvs(
+        loc=F2XCO2_MEAN,
+        scale=F2XCO2_NINETY / NINETY_TO_ONESIGMA,
+        size=SAMPLES,
+        random_state=SEEDS[73],
+    )
+
+    ecs = -f2x / samples.lamg
+    tcr = f2x / (-samples.lamg + samples.gamma_2l * samples.eff)
+
+    # create all the configs as a dataframe
+    config_table = pd.DataFrame(
+        dict(r0=r0_fs, rc=rC_fs, rt=rT_fs, F2x=f2x, tcr=tcr, ecs=ecs)
+    )
+    print("First 5 configs:")
+    print(config_table.head())
+
+    def make_config_dict(row):
+        # take a pandas itertuples row and convert it into a config dict
+        config = row._asdict()
+        # add our forcings
+        config['C'] = C
+        config['other_rf'] = -co2_log(C, Cpi, row.F2x)
+        return config
+
+    print(f"Running {SAMPLES} ensemble members on {N_PROCS} cores:")
+    rows = config_table.itertuples(index=False)
+    configs = (make_config_dict(row) for row in rows)
+    pconfigs = tqdm(configs, total=SAMPLES, ncols=80, file=sys.stdout)
+    with multiprocessing.Pool(N_PROCS, maxtasksperchild=1000) as pool:
+        batch = pool.imap(run_model, enumerate(pconfigs))
+        stream_ensemble(outfile, batch)
+
+    print(f"Done. Output saved to {outfile}")
+
+if __name__ == '__main__':
+    main()
